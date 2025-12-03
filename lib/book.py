3a4219b784a77bf6dcc53bec9c3e98e2ce2983cb
Craig Omore <craiggomore@gmail.com>
2025-12-03 21:52:57 +0300

    labs

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # title property
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if type(value) is not str:
+            raise TypeError("title must be a string")
+        self._title = value
+
+    # page_count property
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) is not int:
+            raise TypeError("page_count must be an integer")
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = None
+
+    # brand property
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if type(value) is not str:
+            raise TypeError("brand must be a string")
+        self._brand = value
+
+    # size property
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if type(value) not in [int, float]:
+            print("size must be an integer")
+            return
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
